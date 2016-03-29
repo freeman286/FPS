b1886fceeaf4951e444483d158f1a8d9c2cf3779
freeman286 <molemayer@gmail.com>
2016-03-29 17:06:06 +0200

    Add sniper

---
+++ Assets/Scripts/RenderCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RenderCamera : MonoBehaviour {
+
+    private Texture2D renderedTexture;
+
+    public Material mat;
+
+    void Awake() {
+        renderedTexture = new Texture2D(Screen.width, Screen.height);
+
+        mat.mainTexture = renderedTexture;
+    }
+
+    void OnPostRender () {
+
+        renderedTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+
+        renderedTexture.Apply();
+    }
+}
