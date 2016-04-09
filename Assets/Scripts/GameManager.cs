6e67118b462aff3d4dff1d04f85b4553cd5eca8b
freeman286 <molemayer@gmail.com>
2016-04-09 20:28:14 +0200

    Add player names

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public class GameManager : MonoBehaviour {
 
@@ -23,6 +24,7 @@ public class GameManager : MonoBehaviour {
     private static Dictionary<string, Player> players = new Dictionary<string, Player>();
     private static Dictionary<string, int> kills = new Dictionary<string, int>();
     private static Dictionary<string, int> deaths = new Dictionary<string, int>();
+    private static Dictionary<string, string> names = new Dictionary<string, string>();
 
     public static void RegisterPlayer(string _netID, Player _player) {
         string _playerID = PLAYER_ID_PREFIX + _netID;
@@ -30,11 +32,13 @@ public class GameManager : MonoBehaviour {
         _player.transform.name = _playerID;
         kills.Add(_playerID, 0);
         deaths.Add(_playerID, 0);
+        names.Add(_playerID, CreateNewName(_playerID));
     }
 
     public static void UnRegisterPlayer(string _playerID) {
         players.Remove(_playerID);
         kills.Remove(_playerID);
+        names.Remove(_playerID);
     }
 
     public static Player GetPlayer(string _playerID) {
@@ -54,7 +58,7 @@ public class GameManager : MonoBehaviour {
     }
 
     void OnGUI () {
-        GUILayout.BeginArea(new Rect(10, 120, 200, 500));
+        GUILayout.BeginArea(new Rect(10, 120, 500, 700));
         GUILayout.BeginVertical();
 
         GUILayout.Label("My IP: " + Network.player.ipAddress);
@@ -65,7 +69,7 @@ public class GameManager : MonoBehaviour {
         GUILayout.Label("Players in game:");
         foreach (string _playerID in players.Keys) {
 
-            string _text = players[_playerID].transform.name + " Kills:" + kills[_playerID] + " Deaths:" + deaths[_playerID];
+            string _text = names[_playerID] + " Kills:" + kills[_playerID] + " Deaths:" + deaths[_playerID];
 
             if (players[_playerID].isDead) {
                 GUILayout.Label("<color=grey>" + _text + "</color>");
@@ -85,4 +89,33 @@ public class GameManager : MonoBehaviour {
 
     #endregion
 
+    static string CreateNewName(string _playerID) {
+
+        string[] nameSyllables = { "MLG", "leet", "1337", "h4x", "face", "d00d", "kewl", "m8", "m80", "m88", "m9", "pwn", "FTW", " O RLY?", "4TW", "pew", "Pew Pew", "n00b", "nub" };
+        string[] prefixes = { "Teh", "Mc", "Mr", "Teh Real", "Meh", "Lord", "Mastah" };
+
+        Random.seed = int.Parse(Regex.Replace(_playerID, "[^0-9]", "")) * System.DateTime.Now.Day * System.DateTime.Now.Month * System.DateTime.Now.Year;
+
+        string name = "";
+        string syllable;
+        int numberOfSyllablesInFirstName = Random.Range(2, 4);
+        for (int i = 0; i < numberOfSyllablesInFirstName; i++) {
+
+            syllable = nameSyllables[Random.Range(0, nameSyllables.Length)];
+
+            if (Random.value > 0.5 && i != numberOfSyllablesInFirstName - 1) {
+                syllable = syllable.ToUpper();
+            }
+            if (Random.value > 0.5 && i != numberOfSyllablesInFirstName - 1) {
+                syllable += " ";
+            }
+            name += syllable;
+        }
+
+        if (Random.Range(0.0f, 1.0f) > 0.5f) {
+            name = prefixes[Random.Range(0, prefixes.Length)] + name;
+        }
+
+        return name;
+    }
 }
