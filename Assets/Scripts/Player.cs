a3726d2718fe8108be794018c1e56bcaac76fe58
freeman286 <molemayer@gmail.com>
2016-07-10 17:16:50 +0200

    Balance changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -231,8 +231,6 @@ public class Player : NetworkBehaviour {
         altWeaponHolder.transform.parent = transform;
         cam.transform.parent = rigidbodyOnDeath[0].transform;
 
-        RemoveProjectilesRecursively(transform);
-
         string[] _bodyPart = { "Torso", "Skull", "LeftFoot", "RightFoot", "WeaponHolder", "AltWeaponHolder" };
         float[] _mass = { 3f, 2f, 2f, 2f, 2f, 2f };
 
