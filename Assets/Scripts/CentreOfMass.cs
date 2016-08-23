70c43a3026900d9e5ee8a2bc689d16a71ccadcdf
freeman286 <molemayer@gmail.com>
2016-08-23 15:29:00 +0200

    Arrow fixes

---
+++ Assets/Scripts/CentreOfMass.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CentreOfMass : MonoBehaviour {
+    public Vector3 centre;
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centre;
+    }
+}
